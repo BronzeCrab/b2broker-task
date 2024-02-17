f3e7f1828203106e8a4f639e5af3d4a017ca8bad
Alexey Ustinnikov <a.ustinnikov@grfc.ru>
2024-02-17 23:31:34 +0300

    Added migrations.RunPython func to the migration file.

--- b2btask/models.py
+++ b2btask/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 class Wallet(models.Model):
     label = models.CharField(max_length=255)
-    balance = models.DecimalField(max_digits=36, decimal_places=18)
+    balance = models.DecimalField(max_digits=36, decimal_places=18, default=0)
+
+    def __str__(self):
+        return f"Wallet_{self.label}_id={self.id}"
 
 
 class Transaction(models.Model):
     txid = models.CharField(max_length=255, unique=True)
     amount = models.DecimalField(max_digits=36, decimal_places=18)
     wallet = models.ForeignKey(Wallet, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Transaction_{self.txid}_id={self.id}"
